56060381e2dc3a5653b6d082f04096c15d6c9411
Hridayy <73392185+Hridayy@users.noreply.github.com>
2021-09-15 16:15:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-//https://teachablemachine.withgoogle.com/models/Azf1LJp5D/
+function startclassifier(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier= ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/Azf1LJp5D/model.json",ModelReady)}
+    
+    function ModelReady(){
+    classifier.classify(gotResults)}
+
+    function gotResults(){
+
+    }
+    
